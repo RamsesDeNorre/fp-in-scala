0c4077a4b27f108e4476919d24bba56bcb9b9558
RamsesDeNorre <ramses.rommel@gmail.com>
2013-03-17 15:56:04 +0100

    Finish chap 3 exercises.

--- exercises/chap3/ex.scala
+++ exercises/chap3/ex.scala
@@ -4,13 +4,23 @@ import scala.annotation.tailrec
 object Chap3 {
 
   sealed trait List[+A] {
+  
     def :: [B >: A] : B => List[B] = new ::(_, this)
-    override def toString: String = this match {
-      case Nil   => "Nil"
-      case x::xs => x.toString + " :: " + xs.toString
+    
+    override def toString: String = {
+      
+      def toString_ : List[A] => String = {
+        case Nil   => "Nil"
+        case x::xs => x.toString + " :: " + toString_ (xs)
+      }
+
+      "(" + toString_ (this) + ")"
     }
+
   }
+
   case object Nil extends List[Nothing]
+  
   case class ::[+A](head: A, tail: List[A]) extends List[A]
 
   object List {
@@ -104,6 +114,7 @@ object Chap3 {
     def length [A] : List[A] => Int =
       foldRight (_, 0) ((_, l) => l + 1)
 
+    @tailrec
     def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B =
       l match {
         case Nil   => z
@@ -178,8 +189,95 @@ object Chap3 {
     def filter_ [A] (p: A => Boolean) : List[A] => List[A] =
       flatMap (x => if (p(x)) x::Nil else Nil)
 
+    def zipWith [A,B,C] (f: A => B => C) : List[A] => List[B] => List [C] = {
 
+      def zipWith_ (f: A => B => C) : (List[A], List[B]) => List[C] = {
+        case (x::xs,y::ys) => f (x) (y) :: zipWith_ (f) (xs,ys)
+        case _             => Nil
+      }
+
+      curry (zipWith_ (f))
+    }
+
+
+     /*******************/
+    // hasSubSequences //
+
+    def tails [A] : List[A] => List[List[A]] = {
+      case Nil     => Nil::Nil
+      case l@_::xs => l :: tails (xs)
+    }
+
+    def isPrefixOf [A] : List[A] => List[A] => Boolean = {
+      @tailrec
+      def isPrefixOf_ [A] (pre: List[A], l: List[A]) : Boolean = (pre,l) match {
+        case (Nil,_)                 => true
+        case (x::xs,y::ys) if x == y => isPrefixOf_ (xs,ys)
+        case _                       => false
+      }
+
+      curry (isPrefixOf_)
+    }
+
+    def any [A] (p: A => Boolean): List[A] => Boolean =
+      foldRight (_,false) (p(_) || _)
+
+    def hasSubSequence [A] (sub: List[A]) : List[A] => Boolean =
+      (any (isPrefixOf (sub))) compose (tails)
 
   }
+
+  sealed trait Tree[+A]
+  case class Leaf[A](value: A) extends Tree[A]
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  object Tree {
+
+    def size [A] : Tree[A] => Int = {
+      case Leaf   (_)   => 0
+      case Branch (l,r) => 1 + size (l) + size (r)
+    }
+
+    def maximum : Tree[Int] => Int = {
+      case Leaf   (l)   => l
+      case Branch (l,r) => maximum (l) max maximum (r)
+    }
+
+    def depth [A] : Tree[A] => Int = {
+      case Leaf   (_)   => 1
+      case Branch (l,r) => 1 + (depth (l) max depth (r))
+    }
+
+    def map [A,B] (f: A => B) : Tree[A] => Tree[B] = {
+      case Leaf   (l)   => Leaf (f(l))
+      case Branch (l,r) => Branch (map (f) (l), map (f) (r))
+    }
+
+    def fold [A,B] (f: A => B) (h: B => B => B) : Tree[A] => B = {
+      case Leaf   (l)   => f (l)
+      case Branch (l,r) => h (fold (f) (h) (l)) (fold (f) (h) (r))
+    }
+
+    def fold_ [A,B] (f: A => B) (h: (B,B) => B) : Tree[A] => B =
+      fold (f) (h.curried)
+
+    def const [A,B] : A => B => A = a => _ => a
+
+    def id [A] : A => A = a => a
+
+    def size_ [A] : Tree[A] => Int =
+      fold_ (const (0)) (_ + _ + 1)
+
+    def maximum_ : Tree[Int] => Int =
+      fold_[Int,Int] (id) (_ max _)
+
+    def depth_ [A] : Tree[A] => Int =
+      fold_ (const (1)) ((x,y) => (x max y) + 1)
+
+    def map_ [A,B] (f: A => B) : Tree[A] => Tree[B] =
+      fold_[A,Tree[B]] ((Leaf (_:B)) compose f) (Branch (_, _))
+
+  }
+
 }
 
